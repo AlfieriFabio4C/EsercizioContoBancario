793ddef4a7103364d776dea3a4a30a1a4a9a2e9a
AlfieriFabio4C <56158271+AlfieriFabio4C@users.noreply.github.com>
2019-11-15 13:15:55 +0100

    Add files via upload

---
+++ ContoEsteso.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eserciziocontobancario;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alfieri.fabio
+ */
+public class ContoEsteso extends ContoBancario {
+
+    protected int fido;
+
+    public ContoEsteso(String numeroConto) {
+        super(numeroConto);
+        this.fido = fido = 1000;
+    }
+
+    public ContoEsteso(int bilancio, String numeroConto) {
+        super(bilancio, numeroConto);
+    }
+
+    public ContoEsteso(int bilancio, int fido, String AlfaNumerico) {
+        super(bilancio, AlfaNumerico);
+        this.fido = fido;
+
+    }
+
+    public int getFido() {
+        return fido;
+    }
+
+    public void setFido(int fido) {
+    }
+
+    @Override
+    public int prelievo() {
+        int soldi;
+        bilancio = bilancio + fido;
+        int scelta = 0;
+        do {
+            soldi = Integer.parseInt(JOptionPane.showInputDialog("Inserire i soldi da prelevare"));
+            if (bilancio > soldi) {
+                bilancio = bilancio - soldi;
+            } else {
+                System.out.println("I soldi da prelevare superano il suo conto.");
+            }
+            scelta = JOptionPane.showConfirmDialog(null,"Vuoi continuare a prelevare i soldi ?");
+        } while (scelta == 0);
+        return bilancio;
+    }
+
+}
---
+++ EsercizioContoBancario.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package eserciziocontobancario;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author alfieri.fabio
+ */
+public class EsercizioContoBancario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        ContoEsteso a = new ContoEsteso(20, 30, "365");
+        System.out.println(a.getBilancio());
+        System.out.println(a.getNumeroConto());
+        System.out.println(a.getFido());
+        a.deposito();
+        System.out.println(a.getBilancio());
+        a.prelievo();
+        System.out.println(a.getBilancio());
+    }
+}
